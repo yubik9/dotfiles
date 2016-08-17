afe76d061e867189a757df9c018d0d98efe2227b
Jimmy Fan <fanminjian@gmail.com>
2016-08-17 21:25:58 +1000

    Add gpg-agent

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.irbrc ~/.irbrc
 
+mkdir -p ~/.gnupg
+ln -s ~/dotfiles/gpg-agent.conf ~/.gnupg/gpg-agent.conf
+
 git submodule init
 git submodule update
