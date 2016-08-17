1119ab9a406f649a939506f6b00a088aceee6df6
Jimmy Fan <fanminjian@gmail.com>
2016-08-17 21:29:29 +1000

    Add gemrc

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.gemrc ~/.gemrc
 ln -s ~/dotfiles/.irbrc ~/.irbrc
 
 mkdir -p ~/.gnupg
